6abc736785a0a42f4687a0925efd18d96a752cfa
Sudarshan Regmi <sudarshan.regmi@imark.com.np>
2023-02-05 16:51:17 +0545

    empty incremental file added for Kalimoni Hospital to track all further SQL changes

---
+++ Database/4. incremental 3Feb2023++/1.1-Incremental for Kalimoni Hospital.sql
@@ -0,0 +1,4 @@
+--Start:Bikesh:5Feb2023---Add/Update Departments as per Kalimoni Hospital---
+   ---Write your incrementals in this section
+--End:Bikesh:5Feb2023---Add/Update Departments as per Kalimoni Hospital---
+
